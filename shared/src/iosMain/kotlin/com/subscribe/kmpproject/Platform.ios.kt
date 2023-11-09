3ef0fa640993e93bf9f031c70ada87e5fef6d840
xiezhihua <xiezhihua@beemwork.com>
2023-11-09 17:07:20 +0800

    feat: init project

---
+++ shared/src/iosMain/kotlin/com/subscribe/kmpproject/Platform.ios.kt
@@ -0,0 +1,9 @@
+package com.subscribe.kmpproject
+
+import platform.UIKit.UIDevice
+
+class IOSPlatform: Platform {
+    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+}
+
+actual fun getPlatform(): Platform = IOSPlatform()
